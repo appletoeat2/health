94fa83eb9fab8bf2739a97d53edcc204c301fdd7
appletoeat2 <waqas.rehman.ce@gmail.com>
2014-01-02 21:48:58 +0500

    update

--- javascript/products_filter.js
+++ javascript/products_filter.js
@@ -1,33 +1,125 @@
+function run_filter()
+{
+	if($("#main_product_group").attr("checked")){
+		$(".group_div").css("display", "block") ;
+		$(".product_group").attr("checked", false) ;
+	} else {
+		$(".group_div").css("display", "none") ;
+	}
+	/**/
+	
+	$(".product_group").each(function(){
+		if($(this).attr("checked")) {
+			var _id = "#"+"group_"+($(this).attr("id")) ;
+			$(_id).css("display", "block") ;
+		}
+	}) ;
+	/**/
+	
+	$(".product_item").css("display","block") ;
+	
+	$(".product_category").each(function(){
+		var id = $(this).attr("id") ;
+		if($(this).attr("checked")) {
+			$(".product_item").each(function(){
+				var categories = $(this).attr("categories") ;
+				if(!in_array(categories, id)) $(this).css("display","none") ;
+			});
+		}
+	});
+	/**/
+	
+	$(".food_sensitivity").each(function(){
+		var id = $(this).attr("id") ;
+		if($(this).attr("checked")) {
+			$(".product_item").each(function(){
+            	var food_sensitivity = $(this).attr("food_sensitivities") ;
+            	if(!in_array(food_sensitivity, id)) $(this).css("display","none") ;
+			});
+		} 
+	});
+	/**/
+	
+	$(".group_div").each(function(){
+		var display_flag = false ;
+		var chk = "input:checkbox#" + split_group($(this).attr("id")) ;
+		$(this).children("div.product_item").each(function(){
+			if(($(this).css("display") == "block") && (($(chk).attr("checked")) || ($("#main_product_group").attr("checked"))))
+				display_flag = true ;
+		});
+		if(display_flag) $(this).css("display","block") ;
+		else $(this).css("display","none") ;
+	}) ;
+	/**/
+}
+$(function(){
+	$("#main_product_group").change(function(){
+		run_filter() ;
+	}) ;
+	$(".product_group").change(function(){
+		$("#main_product_group").attr("checked", false) ;
+		run_filter() ;
+	}) ;
+	$(".product_category").change(function(){
+		run_filter() ;
+	}) ;
+	$(".food_sensitivity").change(function(){
+		run_filter() ;
+	}) ;
+}) ;
+
+
+function in_array(string, id)
+{
+	if(string == "") return false ;
+	else
+	{
+		string_array = string.split(",") ;
+		for(var i = 0 ; i < string_array.length ; i++)
+		{
+			if(string_array[i] == id)
+				return true ;
+		}
+		return false ;
+	}
+}
+
+function split_group(group_id)
+{
+	array = group_id.split("_") ;
+	return array[1] ; 
+}
+
+/*
 $(function(){
 	
 	$("#main_product_group").change(function(){
 		if($(this).attr("checked")) {
 			$(".group_div").css("display", "block") ;
-			$(".product_group").attr("checked", false) ;
+			$(".product_group").attr("checked", false) ;		
 		} else {
 			$(".group_div").css("display", "none") ;
 		}
+		adjust_filter() ;
+		display_group() ;
 	}) ;
 	
 	$(".product_group").change(function(){		
 		if($(this).attr("checked")) {
-			var _id = "#"+"group_"+($(this).attr("id")) ;
 			$("#main_product_group").attr("checked", false) ;
 			$(".group_div").css("display", "none") ;
-			
 			$(".product_group").each(function(){
-				if($(this).attr("checked"))
+				if($(this).attr("checked")) {
 					var _id1 = "#"+"group_"+($(this).attr("id")) ;
 					$(_id1).css("display", "block") ;
+				}
 			}) ;
-			display_group() ;
-			
 		} else {
 			var _id = "#"+"group_"+($(this).attr("id")) ;
         	$(_id).css("display", "none") ;
         }
-	});
-	
+		adjust_filter() ;
+	}) ;
 	
 	$(".product_category").change(function(){
 		var my_id = $(this).attr("id") ;
@@ -69,7 +161,6 @@ $(function(){
 		}
 		display_group() ;
 	});
-	
 }) ;
 
 function display_group()
@@ -78,7 +169,8 @@ function display_group()
 		var display_flag = false ;
 		
 		$(this).children("div.product_item").each(function(){
-        	if($(this).css("display") == "block") 
+			var chk = "checkbox#" + split_group($(this).attr("id")) ;
+			if($(this).css("display") == "block" && ($(chk).attr("checked") || $("#main_product_group").attr("checked")))
 				display_flag = true ;
 		});
 		
@@ -133,18 +225,4 @@ function if_all_checked(_class)
 	if(false_flag == 1) return false ;
 	else return true ;
 }
-
-function in_array(string, id)
-{
-	if(string == "") return false ;
-	else
-	{
-		string_array = string.split(",") ;
-		for(var i = 0 ; i < string_array.length ; i++)
-		{
-			if(string_array[i] == id)
-				return true ;
-		}
-		return false ;
-	}
-}
+/**/

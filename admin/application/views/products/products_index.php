7de915d39e80aa57fb4dece7d7abf3a5535f6a9e
Ryan Boyington <strategicdesign@mac.com>
2014-01-01 20:51:29 -0500

    Misc Admin Css Changes

--- admin/application/views/products/products_index.php
+++ admin/application/views/products/products_index.php
@@ -1,17 +1,18 @@
 <div id="right">
 	<div class="section">
     	<div class="box">
-            <div class="title">Products Detail<span class="hide"></span></div>
+            <div class="title">Products<span class="hide"></span></div>
             <div class="content">
             	<table cellspacing="0" cellpadding="0" border="0" class="all"> 
 					<thead> 
 						<tr>
 							<th width="25%">Product Name</th>
-							<th width="20%">Product Code</th>
+							<th width="15%">Product Code</th>
 							<th width="25%">Health Claim</th>
-							<th width="10%">Is New</th>
-							<th width="10%">Status</th>
-							<th width="10%">Action</th>
+							<th width="8%">Is New</th>
+							<th width="8%">Status</th>
+							<th width="7%">Sort</th>
+							<th width="12%">Action</th>
 						</tr>
 					</thead>
 				<?php if($products) { ?>	
@@ -23,6 +24,7 @@
 							<td><?php echo stripslashes($rec->health_claim) ; ?></td>
 							<td><?php echo stripslashes($rec->isnew) ; ?></td>
 							<td><?php echo stripslashes($rec->status) ; ?></td>
+							<td><?php echo stripslashes($rec->sort_order) ; ?></td>
 							<td><a href="<?php echo base_url()."products/product_detail/".$rec->id ; ?>">View</a> - <a href="<?php echo base_url()."products/edit_product/".$rec->id ; ?>">Edit</a></td>
 						</tr>
                     <?php endforeach ; ?>

12de7230ae5a36ed540d3bd32aabf05f7d91ca4e
appletoeat2 <waqas.rehman.ce@gmail.com>
2014-01-29 06:57:44 +0500

    update

--- application/controllers/resources.php
+++ application/controllers/resources.php
@@ -51,20 +51,22 @@ class Resources extends CI_Controller
 	{
 		if($_POST)
 		{
-			$flag = false ;
-			$attributes = array() ;
-			for($i = 1 ; $i < 55 ; $i++)
+			$validation_parameters = $this->make_array() ;
+			if(form_validation_function($validation_parameters) == FALSE)
 			{
-				$attributes["q".$i] = $this->input->post("q".$i) ; 
-				
-				if($attributes["q".$i] == "")
-					$flag = true ; 
-			}
-			
-			if($flag) {
 				$data["error"] = 1 ;
 				$this->load->view('template/body', array_merge($data, $this->load_data("resources/candida_questionnaire"))) ;
-			} else $this->load->view("template/body", array_merge($attributes, $this->load_data("resources/candida_results"))) ;
+			}
+			else
+			{
+				$attributes = array() ;
+				for($i = 1 ; $i < 55 ; $i++)
+				{
+					$attributes["q".$i] = $this->input->post("q".$i) ;
+					if($attributes["q".$i] == "") $flag = true ; 
+				}
+				$this->load->view("template/body", array_merge($attributes, $this->load_data("resources/candida_results"))) ;
+			}
 		}
 		else
 		{
@@ -103,5 +105,21 @@ class Resources extends CI_Controller
 		$data["view"] = $view ;
 		
 		return $data ;
+	}
+	
+	private function make_array()
+	{
+	  	$headings = array("Abdominal Pain", "Anxiety or tearfulness", "Bad breath", "Burning/tearing of eyes", "Chronic sore throat", "Confusion", "Cough or recurrent bronchitis", "Cravings for alcohol", "Cravings for bread", "Cravings for sweets", "Diarrhea", "Difficulty with decision making", "endometriosis or infertility", "Fatigue", "Frequent colds and flues", "Frequent indigestion", "Headaches", "Heartburn", "Hives", "Impotence", "Inability to concentrate", "Insomnia", "Itchy ears/nose", "Jitteriness/irritability", "Loss of balance", "Low libido", "Menstrual irregularities", "Mood swings", "Mucus in stools", "Multiple food sensitivities", "Muscle aches", "Muscle weakness", "Nasal/sinus congestion", "Numbness, burning pain or tightness in chest", "PMS", "Poor coordination", "Poor memory", "Poor sense of direction", "Post nasal drip", "Prostatitis", "Rashes or psoriasis", "Rectal itching", "Recurrent ear infections", "Sensitivity to foods leavened with yeast", "Sensitivity to mould", "Sensitivity to perfume, paints chemicals", "Sensitivity to tobacco smoke", "Shaking or irritable", "Spacey feeling", "Strong body odour", "Swollen or painful joints", "Thrush in mouth", "Vaginal infections", "White coating on tongue") ;
+		
+		$validation_array = array() ;
+		
+		$main_counter = 1 ;
+		
+		foreach($headings as $rec) :
+			$validation_array["q".$main_counter] = $rec."&required" ;  
+			$main_counter = $main_counter + 1 ;
+      	endforeach ;
+		//print_r($validation_array) ; exit ;
+		return $validation_array ;
 	}
 }

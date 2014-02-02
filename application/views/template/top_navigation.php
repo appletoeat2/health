05216962c9708aa1e4def1b64118bb53064b5856
Ryan Boyington <strategicdesign@mac.com>
2014-02-02 09:03:47 -0500

    Ryan

--- application/views/template/top_navigation.php
+++ application/views/template/top_navigation.php
@@ -40,7 +40,7 @@
             			<!--<li><a href="<?php echo base_url("")."resources/heart_to_heart" ; ?>">Heart to heart</a></li>
             			<li><a href="<?php echo base_url("")."resources/brain_power" ; ?>">Brain power</a></li>
             			<li><a href="<?php echo base_url("")."resources/bone_and_braun" ; ?>">Bone and braun</a></li>-->
-            			<li><a href="<?php echo base_url("")."resources/probiotics101" ; ?>">Probiotics 101</a></li>
+            			<!--<li><a href="<?php echo base_url("")."resources/probiotics101" ; ?>">Probiotics 101</a></li>-->
             			<li><a href="<?php echo base_url("")."resources/candida_questionnaire" ; ?>">Candida questionnaire</a></li>
             			<li><a href="<?php echo base_url("")."resources/healthy_journal" ; ?>">Healthy Journal</a></li>
             			<!--<li><a href="<?php echo base_url("")."resources/research_and_articles" ; ?>">Research and Articles</a></li>-->

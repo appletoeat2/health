ba87bfcc96076e9c7ffae1e536f9d3c4e5d02276
appletoeat2 <waqas.rehman.ce@gmail.com>
2014-02-02 09:28:53 +0500

    update

--- application/views/products/filter_menu.php
+++ application/views/products/filter_menu.php
@@ -1,8 +1,20 @@
 <aside class="four columns sidebar leftside">
         <h4 class="subtitle">Health Group</h4>
         <div class="listbox1">
-        	<ul>
-            	<?php
+        	<table>
+            <?php
+			if($product_group_recs) 
+			{
+				echo '<tr><td><input type="checkbox" id="main_product_group" name="" class="" checked="checked" /></td><td> &nbsp; View All</td></tr>' ;
+				foreach($product_group_recs as $rec):
+					echo '<tr><td><input type="checkbox" id="'.$rec->id.'" name="" class="product_group" /></td><td> &nbsp;'.$rec->group_title.'</td></tr>' ;
+				endforeach ;
+			}
+			?>
+            </table>
+            <!--<ul>
+            <?php
+			/*
 				if($product_group_recs) 
 				{
 			echo '<li>
@@ -14,16 +26,28 @@
 					foreach($product_group_recs as $rec):
 						echo '<li><label><input type="checkbox" id="'.$rec->id.'" name="" class="product_group" /><div id="label">'.$rec->group_title.'</div></label></li>' ;
 					endforeach ;
-				}
+				}/**/
 				?>
-            </ul>
+            </ul>-->
 		</div><!-- end-listbox1 -->
         <br class="clear">
         
         <h4 class="subtitle">Categories</h4>
         <div class="listbox1">
-        	<ul>
+        	<table>
+            <?php
+			if($product_category_recs) 
+			{
+				echo '<tr><td><a class="view_all_product_categories" href="javascript:void(0);">View All</a></td><td></td></tr>' ;
+				foreach($product_category_recs as $rec):
+					echo '<tr><td><input type="radio" id="'.$rec->id.'" name="categories_id" class="product_category" /></td><td>'.$rec->category_name.'</td></tr>' ;
+				endforeach ;
+			}
+			?>
+            </table>
+            <!--<ul>
             	<?php
+				/*
 				if($product_category_recs) 
 				{
 					echo '<li><a class="view_all_product_categories" href="javascript:void(0);">View All</a></li>' ;
@@ -31,16 +55,27 @@
 					foreach($product_category_recs as $rec):
 						echo '<li><label><input type="radio" id="'.$rec->id.'" name="categories_id" class="product_category" /><div id="label">'.$rec->category_name.'</div></label></li>' ;
 					endforeach ;
-				}
+				}/**/
 				?>
-        	</ul>
+        	</ul>-->
       	</div><!-- end-listbox1 -->
       	<br class="clear">
       	
         <h4 class="subtitle">FOOD SENSITIVITIES</h4>
       	<div class="listbox1">
-        	<ul>
-                <?php
+        	<table>
+            <?php
+			if($food_sensitivity_recs) 
+			{
+				echo '<tr><td><a class="view_all_food_sensitivites" href="javascript:void(0);">View All</a></td><td></td></tr>' ;
+				foreach($food_sensitivity_recs as $rec):
+				 echo '<tr><td><input type="radio" id="'.$rec->id.'" name="food_sensitivity_id" class="food_sensitivity" /></td><td>'.$rec->name.'</td></tr>' ;
+				endforeach ;
+			}/**/
+			?>
+            </table>
+            <!--<ul>
+                <?php /*
 				if($food_sensitivity_recs) 
 				{
 		//echo '<li><input type="checkbox" id="main_food_sensitivites" name="" class="" checked="checked" /><a href="javascript:void(0);">View All</a></li>' ;
@@ -48,8 +83,8 @@
 					foreach($food_sensitivity_recs as $rec):
 						echo '<li><label><input type="radio" id="'.$rec->id.'" name="food_sensitivity_id" class="food_sensitivity" /><div id="label">'.$rec->name.'</div></label></li>' ;
 					endforeach ;
-				}
+				}/**/
 				?>
-        	</ul>
+        	</ul>-->
       	</div><!-- end-listbox1 -->
 	</aside><!-- end-sidebar-->

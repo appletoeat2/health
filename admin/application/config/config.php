6c3e9f6d29f385e8c8729917311d356722241990
Ryan Boyington <strategicdesign@mac.com>
2014-02-04 02:11:34 -0500

    Ryans late night sync

--- admin/application/config/config.php
+++ admin/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/health/admin/';
+$config['base_url']	= 'http://www.innovitehealth.com/admin/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/template/slider.php
+++ application/views/template/slider.php
@@ -3,7 +3,7 @@
         	<li><a href="<?php echo base_url()."resources/current_promotions" ; ?>"><img src="<?php echo base_url() ; ?>images/slider/heart-health.jpg" alt=""></a></li>
   	    	<li><a href="<?php echo base_url()."products/product_details/6/44" ; ?>"><img src="<?php echo base_url() ; ?>images/slider/mood.jpg" alt=""></a></li>
   	    	<li><a href="<?php echo base_url()."products/product_details/4/51" ; ?>"><img src="<?php echo base_url() ; ?>images/slider/bone.jpg" alt=""></a></li>
-			<li><a href="<?php echo base_url()."products/product_details/5/27" ; ?>"><img src="<?php echo base_url() ; ?>images/slider/digestive.jpg" alt=""></a></li>
-			<li><a href="<?php echo base_url()."products/product_details/0/5" ; ?>"><img src="<?php echo base_url() ; ?>images/slider/anti-aging.jpg" alt=""></a></li>
+			<li><a href="<?php echo base_url()."products/product_details/0/5" ; ?>"><img src="<?php echo base_url() ; ?>images/slider/digestive.jpg" alt=""></a></li>
+			<li><a href="<?php echo base_url()."products/product_details/5/27" ; ?>"><img src="<?php echo base_url() ; ?>images/slider/anti-aging.jpg" alt=""></a></li>
   		</ul>
 	</div>

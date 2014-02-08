59ee949f2370b318167b498b1933bbdb10cf8194
Ryan Boyington <strategicdesign@mac.com>
2014-02-08 08:58:24 -0500

    Added new store list styles and div code

--- application/views/resources/heart_to_heart.php
+++ application/views/resources/heart_to_heart.php
@@ -7,17 +7,110 @@
 <section class="container page-content">
 	<hr class="vertical-space2">
     <section class="sixteen columns">
-		<div class="shop-wrap">
-        	<figure class="shop-item two_third">
-          		<h2>This is a typical Title</h2>
-                <h4>Typical subheading</h4>
-                <p>NPN</p>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem  accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab  illo inventore veritatis et quasi architecto beatae vitae dicta sunt  explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut  odit aut fugit, sed quia consequuntur magni dolores eos qui ratione  voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum  quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam  eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat  voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam  corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?  Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse  quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo  voluptas nulla pariatur?</p>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem  accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab  illo inventore veritatis et quasi architecto beatae vitae dicta sunt  explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut  odit aut fugit, sed quia consequuntur magni dolores eos qui ratione  voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum  quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam  eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat  voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam  corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?  Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse  quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo  voluptas nulla pariatur?</p>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem  accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab  illo inventore veritatis et quasi architecto beatae vitae dicta sunt  explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut  odit aut fugit, sed quia consequuntur magni dolores eos qui ratione  voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum  quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam  eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat  voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam  corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?  Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse  quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo  voluptas nulla pariatur?</p>
-  				<hr class="vertical-space2">
-			</figure>
-		</div>
+		<div class="store-list">
+<hr>
+<div class="one_fifth"> <strong>Natural Choice Healing House</strong><br>
+  14 km away</div>
+<div class="one_fifth">
+(705) 435-2300
+</div>
+<div class="one_fifth">
+2943 Major Mackenzie Dr
+<br>
+Vaughan, ON, L6A 1C6
+</div>
+<div class="one_fifth">
+<a target="_blank" href="https://maps.google.ca/maps?saddr=&daddr=2943 Major Mackenzie Dr Vaughan ON L6A 1C6">Directions</a>
+|
+<a target="_blank" href="http://www.natures-source.com">Website</a>
+</div>
+<div class="one_fifth column-last">
+<a target="_blank" href="https://www.facebook.com/natsrc">
+<img src="http://www.innovitehealth.com/images/social-icons/facebook.png">
+</a>
+<a target="_blank" href="https://twitter.com/Natures_Source">
+<img src="http://www.innovitehealth.com/images/social-icons/twitter.png">
+</a>
+</div>
+</div>
+<div class="store-list">
+<hr>
+<div class="one_fifth"> <strong>Natural Choice Healing House</strong><br>
+  14 km away</div>
+<div class="one_fifth">
+(705) 435-2300
+</div>
+<div class="one_fifth">
+2943 Major Mackenzie Dr
+<br>
+Vaughan, ON, L6A 1C6
+</div>
+<div class="one_fifth">
+<a target="_blank" href="https://maps.google.ca/maps?saddr=&daddr=2943 Major Mackenzie Dr Vaughan ON L6A 1C6">Directions</a>
+|
+<a target="_blank" href="http://www.natures-source.com">Website</a>
+</div>
+<div class="one_fifth column-last">
+<a target="_blank" href="https://www.facebook.com/natsrc">
+<img src="http://www.innovitehealth.com/images/social-icons/facebook.png">
+</a>
+<a target="_blank" href="https://twitter.com/Natures_Source">
+<img src="http://www.innovitehealth.com/images/social-icons/twitter.png">
+</a>
+</div>
+</div>
+<div class="store-list">
+<hr>
+<div class="one_fifth"> <strong>Natural Choice Healing House</strong><br>
+  14 km away</div>
+<div class="one_fifth">
+(705) 435-2300
+</div>
+<div class="one_fifth">
+2943 Major Mackenzie Dr
+<br>
+Vaughan, ON, L6A 1C6
+</div>
+<div class="one_fifth">
+<a target="_blank" href="https://maps.google.ca/maps?saddr=&daddr=2943 Major Mackenzie Dr Vaughan ON L6A 1C6">Directions</a>
+|
+<a target="_blank" href="http://www.natures-source.com">Website</a>
+</div>
+<div class="one_fifth column-last">
+<a target="_blank" href="https://www.facebook.com/natsrc">
+<img src="http://www.innovitehealth.com/images/social-icons/facebook.png">
+</a>
+<a target="_blank" href="https://twitter.com/Natures_Source">
+<img src="http://www.innovitehealth.com/images/social-icons/twitter.png">
+</a>
+</div>
+</div>
+<div class="store-list">
+<hr>
+<div class="one_fifth"> <strong>Natural Choice Healing House</strong><br>
+  14 km away</div>
+<div class="one_fifth">
+(705) 435-2300
+</div>
+<div class="one_fifth">
+2943 Major Mackenzie Dr
+<br>
+Vaughan, ON, L6A 1C6
+</div>
+<div class="one_fifth">
+<a target="_blank" href="https://maps.google.ca/maps?saddr=&daddr=2943 Major Mackenzie Dr Vaughan ON L6A 1C6">Directions</a>
+|
+<a target="_blank" href="http://www.natures-source.com">Website</a>
+</div>
+<div class="one_fifth column-last">
+<a target="_blank" href="https://www.facebook.com/natsrc">
+<img src="http://www.innovitehealth.com/images/social-icons/facebook.png">
+</a>
+<a target="_blank" href="https://twitter.com/Natures_Source">
+<img src="http://www.innovitehealth.com/images/social-icons/twitter.png">
+</a>
+</div>
+</div>
         <?php $this->load->view("resources/side_links") ; ?>
 	</section><!-- end-main-content --> 
 
--- application/views/stores/index.php
+++ application/views/stores/index.php
@@ -1,25 +1,32 @@
 <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCyGAu05sQeK7UhFmsVqB7S_MEbH20T7ic&sensor=false"></script>
 <script type="text/javascript" src="<?php echo base_url() ; ?>javascript/map_script.js"></script>
 <style type="text/css">
-.text{ font-size:12px;}
-.win{ height:auto ;  overflow: hidden;}
+.text {
+	font-size: 12px;
+}
+.win {
+	height: auto;
+	overflow: hidden;
+}
 </style>
 <section id="headline" class="aboutus">
-	<div class="container shop-item">
-    	<h3><a href="index.php">Find a Retailer </a></h3>
-	</div>
+
+<div class="container shop-item">
+  <h3><a href="index.php">Find a Retailer </a></h3>
+</div>
 </section>
 <section class="container page-content" >
-    <hr class="vertical-space2">
-    <section class="sixteen columns">
+  <hr class="vertical-space2">
+  <section class="sixteen columns">
     <div class="shop-item">
-    <h2>Store Locator</h2>
-    <h4>Enter your address below to find stores near you </h4>
+      <h2>Store Locator</h2>
+      <h4>Enter your address below to find stores near you </h4>
     </div>
     <div class="one_third">
-   	  <label class="vertical-space2" for="address">Address: </label><input type="text" id="address" name="address" value="" />
-	  </div>
-        <!--
+      <label class="vertical-space2" for="address">Address: </label>
+      <input type="text" id="address" name="address" value="" />
+    </div>
+    <!--
         <div class="one_fourth">
             <label class="vertical-space2" for="city_name_dropdown">City: </label>
             <select id="city_name_dropdown"  name="city_name_dropdown">
@@ -29,30 +36,38 @@
 				<?php // endforeach ; }?>
             </select>
         </div>-->
-        <div class="one_third">
-            <label class="vertical-space2" for="radius">Radius: </label>
-            <select id="radius" name="radis">
-                <option value="1">1 KM</option>
-                <option value="5">5 KM</option>
-                <option value="10">10 KM</option>
-                <option value="25" selected="selected">25 KM</option>
-            </select>
-		</div>
-        
-        <div class="one_third column-last">   
-    		<input type="button" id="submit_button" value="Search Locations" />
-    	</div>
-        
-	</section>
-    
-    <div id="googleMap" style="height:380px; width:100%;"></div>
-		<br />
-        <br />
-    <section class="sixteen columns">
+    <div class="one_third">
+      <label class="vertical-space2" for="radius">Radius: </label>
+      <select id="radius" name="radis">
+        <option value="1">1 KM</option>
+        <option value="5">5 KM</option>
+        <option value="10">10 KM</option>
+        <option value="25" selected="selected">25 KM</option>
+      </select>
+    </div>
+    <div class="one_third column-last">
+      <input type="button" id="submit_button" value="Search Locations" />
+    </div>
+  </section>
+  <div id="googleMap" style="height:380px; width:100%;"></div>
+  <br />
+  <br />
+  <section class="sixteen columns">
+    <!--Waqas use this for the store list
+    <div class="store-list">
+      <hr>
+      <div class="one_fifth"> <strong>Natural Choice Healing House</strong><br>
+        (14 km away)</div>
+      <div class="one_fifth">(705) 435-2300 </div>
+      <div class="one_fifth">2943 Major Mackenzie Dr<br>
+        Vaughan, ON, L6A 1C6 </div>
+      <div class="one_fifth"> <a target="_blank" href="https://maps.google.ca/maps?saddr=&daddr=2943 Major Mackenzie Dr Vaughan ON L6A 1C6">Directions</a> | <a target="_blank" href="http://www.natures-source.com">Website</a> </div>
+      <div class="one_fifth column-last"> <a target="_blank" href="https://www.facebook.com/natsrc"> <img src="http://www.innovitehealth.com/images/social-icons/facebook.png"> </a> <a target="_blank" href="https://twitter.com/Natures_Source"> <img src="http://www.innovitehealth.com/images/social-icons/twitter.png"> </a> </div>
+    </div>-->
     <table id="location_details_table" class="table table-hover">
-    	<tbody>
-        </tbody>
+      <tbody>
+      </tbody>
     </table>
-    </section>
-    <hr class="vertical-space2">
+  </section>
+  <hr class="vertical-space2">
 </section>
